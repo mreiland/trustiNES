3e7470e708b992a6972c181a6cf410ac7d2c2d2d
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-15 17:30:10 -0700

    added functions to pack/unpack processor status flags

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -24,3 +24,26 @@ pub struct CpuState {
     decode_register:DecodeInfo,
 }
 
+
+impl CpuState {
+  fn unpack_flags(self: &CpuState) -> u8 {
+        ( (self.C as u8) << 0)
+      | ( (self.Z as u8) << 1)
+      | ( (self.I as u8) << 2)
+      | ( (self.D as u8) << 3)
+      | ( (self.B as u8) << 4)
+      | ( (1           ) << 5) // flag 5 is unused
+      | ( (self.V as u8) << 6)
+      | ( (self.S as u8) << 7)
+  }
+  fn pack_flags(self: &mut CpuState,flags:u8) {
+    self.C        = ( (flags >> 0) & 1) == 1;
+    self.Z        = ( (flags >> 1) & 1) == 1; 
+    self.I        = ( (flags >> 2) & 1) == 1; 
+    self.D        = ( (flags >> 3) & 1) == 1; 
+    self.B        = ( (flags >> 4) & 1) == 1; 
+    // flags 5 is unused
+    self.V        = ( (flags >> 6) & 1) == 1; 
+    self.S        = ( (flags >> 7) & 1) == 1; 
+  }
+}
