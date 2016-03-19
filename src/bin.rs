90a7d3e8d01daeaf0719b3d94489c9e98973b497
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-18 18:39:57 -0700

    for now we don't need to take args input

--- src/bin.rs
+++ src/bin.rs
@@ -6,14 +6,14 @@ pub mod cpu;
 use std::env;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    //let args: Vec<String> = env::args().collect();
     
-    if args.len() < 2 {
-      println!("Usage: main FILE");
-      return;
-    }
+    //if args.len() < 2 {
+      //println!("Usage: main FILE");
+      //return;
+    //}
 
-    println!("Loading {}", args[1]);
+    //println!("Loading {}", args[1]);
     let cpu: cpu::CpuState;
 
     match rom_loader::load_ines("roms/nestest.nes") {
