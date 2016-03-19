7cc997f1f55420be8356d13cdff244fc718826a7
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-18 20:57:09 -0700

    removed commented out code

--- src/bin.rs
+++ src/bin.rs
@@ -6,14 +6,6 @@ pub mod cpu;
 use std::env;
 
 fn main() {
-    //let args: Vec<String> = env::args().collect();
-    
-    //if args.len() < 2 {
-      //println!("Usage: main FILE");
-      //return;
-    //}
-
-    //println!("Loading {}", args[1]);
     let cpu: cpu::CpuState;
 
     match rom_loader::load_ines("roms/nestest.nes") {
