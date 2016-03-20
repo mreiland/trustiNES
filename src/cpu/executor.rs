45a6e8ccf75d9ffe9553a87981b523b7d20732f7
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-19 22:22:20 -0700

    filled out CpuExector, stubbed out its methods

--- src/cpu/executor.rs
+++ src/cpu/executor.rs
@@ -1,3 +1,26 @@
+use cpu::common_defs::OpcodeExecInfo;
+use cpu::state::CpuState;
+
 pub struct CpuExecutor {
-    stub: bool,
+    op_table: Vec<OpcodeExecInfo>,
+}
+
+impl CpuExecutor {
+    pub fn new(opcodes: Vec<OpcodeExecInfo> ) -> CpuExecutor {
+        CpuExecutor {
+            op_table: opcodes,
+        }
+    }
+
+    pub fn fetch_and_decode(self: &CpuExecutor, mut cpu_state: CpuState) -> CpuState {
+        cpu_state
+    }
+    pub fn execute(self: &CpuExecutor, mut cpu_state: CpuState) -> CpuState {
+        cpu_state
+    }
+    pub fn step(self: &CpuExecutor, mut cpu_state: CpuState) -> CpuState {
+        cpu_state = self.fetch_and_decode(cpu_state);
+        cpu_state = self.execute(cpu_state);
+        cpu_state
+    }
 }
