31c126aedc0a09ae5fe36d36d4dfad7fc8c685df
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-19 21:41:47 -0700

    the opcode loader now returns vec instead of hashmap

--- src/cpu/opcode.rs
+++ src/cpu/opcode.rs
@@ -27,23 +27,23 @@ impl From<num::ParseIntError> for OpcodeLoadError {
     }
 }
 
-pub fn load_from_file<P:AsRef<Path>>(file_path: P) -> Result<(HashMap<u16,OpcodeExecInfo>,HashMap<u16,OpcodeDebugInfo>),OpcodeLoadError> {
+pub fn load_from_file<P:AsRef<Path>>(file_path: P) -> Result<(Vec<OpcodeExecInfo>,Vec<OpcodeDebugInfo>),OpcodeLoadError> {
     let mut rdr = try!(csv::Reader::from_file(file_path));
     rdr = rdr.has_headers(true);
     rdr = rdr.flexible(false); // all records are the same length
 
-    let mut exec_info_hash = HashMap::<u16,OpcodeExecInfo>::new();
-    let mut debug_info_hash = HashMap::<u16,OpcodeDebugInfo>::new();
+    let mut exec_info_hash = Vec::<OpcodeExecInfo>::new();
+    let mut debug_info_hash = Vec::<OpcodeDebugInfo>::new();
 
     for rec in rdr.decode() {
         let (opcodeString,name,address_mode_name,len,cycles,page_cycles,notes) : (String,String,String,u8,u8,u8,String) = rec.unwrap();
         let opcode = try!(u16::from_str_radix(&opcodeString[2..],16)); // from_str_radix won't parse 0x
 
         let mut debug_info = OpcodeDebugInfo { opcode : opcode, name : name, address_mode_name : address_mode_name, notes : notes, };
-        debug_info_hash.insert(opcode,debug_info);
+        debug_info_hash.push(debug_info);
 
         let mut exec_info = OpcodeExecInfo { opcode: opcode, len: len, cycles: cycles, page_cycles: page_cycles, };
-        exec_info_hash.insert(opcode,exec_info);
+        exec_info_hash.push(exec_info);
     }
     Ok((exec_info_hash,debug_info_hash))
 }
