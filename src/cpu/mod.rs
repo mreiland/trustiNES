ffc1974e8a1504397e72f4829bccbff1e97fdfde
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-19 21:07:44 -0700

    removed erroneous code

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -2,16 +2,9 @@ pub mod opcode;
 mod state;
 mod executor;
 
-use std::io;
-use std::path::Path;
-use std::num;
-use std::collections::HashMap;
-
-use cpu::opcode::ExecInfo;
-use cpu::opcode::DebugInfo;
-
 pub use self::state::CpuState;
 pub use self::state::DecodeInfo;
 
 
 pub use self::executor::CpuExecutor;
+
