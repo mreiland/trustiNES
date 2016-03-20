81f7a311640d595e480d7f92e6526b119c013921
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-19 21:09:15 -0700

    style updates

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -1,10 +1,13 @@
+//public mods
 pub mod opcode;
+
+//private mods
 mod state;
 mod executor;
 
+// hoisted interfaces
 pub use self::state::CpuState;
 pub use self::state::DecodeInfo;
-
 
 pub use self::executor::CpuExecutor;
 
