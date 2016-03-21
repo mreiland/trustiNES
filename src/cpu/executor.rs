5fd849c648655238e311fb3cde676da6a488da9c
Stephen Davey <stephen.b.davey@gmail.com>
2016-03-22 04:32:00 +1100

    Updating op_table member to be of type HashMap<u16, OpcodeExecInfo>. CpuExecutor::new() still takes a Vec<OpcodeExecInfo>. In case of duplicate opcodes, the latest duplicate takes precedence, and the older one is logged.

--- src/cpu/executor.rs
+++ src/cpu/executor.rs
@@ -1,15 +1,26 @@
 use cpu::common_defs::OpcodeExecInfo;
 use cpu::state::CpuState;
 use memory::Memory;
+use std::collections::HashMap;
 
 pub struct CpuExecutor {
-    op_table: Vec<OpcodeExecInfo>,
+    op_table: HashMap<u16,OpcodeExecInfo>,
 }
 
 impl CpuExecutor {
     pub fn new(opcodes: Vec<OpcodeExecInfo> ) -> CpuExecutor {
+        // Construct the HashMap and copy it over to CpuExecutor's constructor.
+        let mut op_table_temp: HashMap<u16, OpcodeExecInfo> = HashMap::new();
+        for oc in opcodes {
+            if op_table_temp.contains_key(&oc.opcode) {
+                println!("Encountered duplicate opcode: {:#x}", oc.opcode);
+                println!("Replacing previous opcode, which was {{ opcode: {:#x}, len: {}, cycles: {}, page_cycles: {} }}", oc.opcode, oc.len, oc.cycles, oc.page_cycles);
+            }
+            op_table_temp.insert(oc.opcode, oc);
+        }
+        
         CpuExecutor {
-            op_table: opcodes,
+            op_table: op_table_temp,
         }
     }
 
