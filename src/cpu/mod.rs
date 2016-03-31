b0008764af765faeb7751be638b71eb030b91f97
ReconditePhreak <trustiNES@michaelreiland.net>
2016-03-30 22:43:33 -0700

    renamed cpu/executor.rs to cpu/cpu_executor.rs for consistency

--- src/cpu/mod.rs
+++ src/cpu/mod.rs
@@ -3,14 +3,14 @@ pub mod opcode;
 
 //private mods
 mod common_defs;
-mod executor;
+mod cpu_executor;
 mod cpu_state;
 
 // hoisted interfaces
 pub use self::common_defs::OpcodeDebugInfo;
 pub use self::common_defs::OpcodeExecInfo;
 
-pub use self::executor::CpuExecutor;
+pub use self::cpu_executor::CpuExecutor;
 
 pub use self::cpu_state::CpuState;
 pub use self::cpu_state::DecodeRegister;
