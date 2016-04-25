de044f3d448f47923aef43ee22c8046bc43b9091
ReconditePhreak <trustiNES@michaelreiland.net>
2016-04-25 04:29:11 -0700

    cpu load/fetch/log/execution of the nestest rom is implemented

--- src/bin.rs
+++ src/bin.rs
@@ -2,10 +2,11 @@ extern crate byteorder;
 pub mod rom_loader;
 pub mod memory;
 pub mod cpu;
+pub mod logger;
 
-use std::env;
 use cpu::CpuState;
 use memory::Memory;
+
 
 fn main() {
     let rom = rom_loader::load_ines("roms/nestest.nes").unwrap();
@@ -16,9 +17,15 @@ fn main() {
     // TODO: handle this properly instead of just unwrapping
     let opcode_info = cpu::opcode::load_from_file("resources/opcodes.csv").unwrap();
     let executor = cpu::CpuExecutor::new(opcode_info.0);
+    let mut logger = logger::NesTest::new("resources/nestest.out",opcode_info.1);
+
+    executor.power_on(&mut cpu, &mut mem);
+    cpu.pc = 0xC000;
+    mem.write(0,&rom);
 
     for i in 0..10 {
         executor.fetch_and_decode(&mut cpu,&mut mem);
+        logger.log_after_fetch(&cpu,&mem);
         executor.execute(&mut cpu,&mut mem);
     }
 }
