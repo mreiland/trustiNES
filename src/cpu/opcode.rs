c1ae3fb180c5027f7c1288ea9312e6c95bd622d0
ReconditePhreak <trustiNES@michaelreiland.net>
2016-04-26 09:14:47 -0700

    removed unsafe blocks from the opcode file load

--- src/cpu/opcode.rs
+++ src/cpu/opcode.rs
@@ -16,7 +16,7 @@ pub enum OpcodeLoadError {
   ParseOpcodeClass(opcode_class::ParseError),
   ParseAddressMode(address_mode::ParseError),
   DuplicateOpcode(String),
-  TooManyOpcodes
+  IncorrectOpcodeCount(String)
 }
 
 impl From<csv::Error> for OpcodeLoadError {
@@ -45,24 +45,14 @@ pub fn load_from_file<P:AsRef<Path>>(file_path: P) -> Result<(Vec<OpcodeExecInfo
     rdr = rdr.has_headers(true);
     rdr = rdr.flexible(false); // all records are the same length
 
-    let max_opcode_num = 256;
-
-    let mut exec_info_vec = Vec::with_capacity(max_opcode_num);
-    let mut debug_info_vec = Vec::with_capacity(max_opcode_num);
+    let expected_opcode_count = 256;
+    let mut exec_info_vec = Vec::with_capacity(expected_opcode_count);
+    let mut debug_info_vec = Vec::with_capacity(expected_opcode_count);
     let mut duplicate_check = Vec::<u8>::new();
-
-    unsafe {
-        exec_info_vec.set_len( max_opcode_num);
-        debug_info_vec.set_len(max_opcode_num);
-    }
 
     let mut count = 0;
 
     for rec in rdr.decode() {
-        if count > max_opcode_num {
-            return Err(OpcodeLoadError::TooManyOpcodes);
-        }
-
         let (opcode_string,name,address_mode_name,len,cycles,page_cycles,notes) : (String,String,String,u8,u8,u8,String) = rec.unwrap();
         let opcode = try!(u8::from_str_radix(&opcode_string[2..],16)); // from_str_radix won't parse 0x
 
@@ -78,11 +68,17 @@ pub fn load_from_file<P:AsRef<Path>>(file_path: P) -> Result<(Vec<OpcodeExecInfo
 
         let exec_info = OpcodeExecInfo { opcode: opcode, len: len, cycles: cycles, page_cycles: page_cycles, address_mode: address_mode,opcode_class:opcode_class };
 
-        debug_info_vec[opcode as usize] = debug_info;
-        exec_info_vec[opcode as usize] = exec_info;
+        debug_info_vec.push(debug_info);
+        exec_info_vec.push(exec_info);
 
         count = count+1;
     }
+    if exec_info_vec.len() != expected_opcode_count {
+        return Err(OpcodeLoadError::IncorrectOpcodeCount(format!("Expected {} opcodes, found {}",expected_opcode_count,exec_info_vec.len())));
+    }
+
+    exec_info_vec.sort_by(|a,b| u8::cmp(&a.opcode,&b.opcode));
+    debug_info_vec.sort_by(|a,b| u8::cmp(&a.opcode,&b.opcode));
     Ok((exec_info_vec,debug_info_vec))
 }
 
